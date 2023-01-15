ddcc627b32fb4c821f983590ebc68f3a1c722ea9
clemkeirua <clement@keiruaprod.fr>
2023-01-15 21:15:11 +0100

    added all instructions + InMemoryIO

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,120 @@
 // ] if the value of the current cell is non-zero, jump back to the corresponding [
 
 
-fn main() {
+use std::collections::HashMap;
+
+enum Instruction{
+    IncrValue,
+    DecrValue,
+    IncrPointer,
+    DecrPointer,
+    OutputChar,
+    InputChar,
+    JZ,
+    JNZ,
+}
+
+fn parse_brackets(program:&str) -> Option<HashMap<usize, usize>> {
+    let mut brackets_mapping:HashMap<usize, usize> = HashMap::new();
+    let mut par_stack:Vec<(char, usize)> = Vec::new();
+    for (pos, c) in program.chars().enumerate() {
+        match c {
+            '[' => {
+                par_stack.push(('[' as char, pos));
+            },
+            ']' => {
+                match par_stack.pop() {
+                    Some(matching_open) => {
+                        let opening_pos = matching_open.1;
+                        let closing_pos = pos;
+                        brackets_mapping.insert(opening_pos, closing_pos);
+                        brackets_mapping.insert(closing_pos, opening_pos);
+                        // brackets_mapping[*pos] = opening_pos;
+                    },
+                    None => return None
+                }
+            },
+            _ => {}
+        }
+    }
+    Some(brackets_mapping)
+}
+
+trait BrainfuckIo {
+    fn output_char(&mut self, c:&char);
+    fn next_input(&mut self) -> char;
+}
+
+#[derive(Debug)]
+struct InMemoryIO {
+    output:Vec<char>,
+    inputs:Vec<char>,
+}
+
+impl Default for InMemoryIO{
+    fn default() -> Self {
+        InMemoryIO{
+            output:Vec::new(),
+            inputs:Vec::new(),
+        }
+    }
+}
+
+impl BrainfuckIo for InMemoryIO {
+    fn output_char(&mut self, c: &char) {
+        self.output.push(*c);
+    }
+
+    fn next_input(&mut self) -> char {
+        todo!()
+    }
+}
+
+fn run_program(program:&str, io:&mut impl BrainfuckIo) {
     let mut memory = [0u8;256];
     let mut cell_id:usize = 0;
     let mut ip = 0usize;
-    let sample = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++."; // prints "Z"
-    while true {
-        if ip < sample.len() {
-            match sample.chars().nth(ip) {
-                Some('+') => { memory[cell_id] += 1; },
+    let brackets_mapping = parse_brackets(&program).unwrap();
+    loop {
+        if ip < program.len() {
+            match program.chars().nth(ip) {
+                Some('+') => {
+                    // + Increment the value of the cell by 1
+                    memory[cell_id] += 1;
+                },
+                Some('-') => {
+                    // - Decrement the value of the cell by 1
+                    memory[cell_id] -= 1;
+                },
+                Some('>') => {
+                    // > Move the pointer to the next cell to the right
+                    cell_id += 1;
+                },
+                Some('<') => {
+                    // < Move the pointer to the next cell to the left
+                    cell_id -= 1;
+                },
                 Some('.') => {
-                    let v = memory[cell_id] as char;
-                    println!("{v}"); },
+                    // . Output the ASCII character corresponding to the value of the current cell
+                    let v:char = memory[cell_id] as char;
+                    io.output_char(&v);
+                },
+                Some(',') => {
+                    // , Input a character and store its ASCII value in the current cell
+                    unimplemented!(", Input a character and store its ASCII value in the current cell");
+                }
+                Some('[') => {
+                    // [ If the value of the cell is zero, jump to the corresponding ] character
+                    if memory[cell_id] == 0{
+                        ip = *brackets_mapping.get(&ip).unwrap();
+                    }
+                },
+                Some(']') => {
+                    // ] if the value of the current cell is non-zero, jump back to the corresponding [
+                    if memory[cell_id] != 0{
+                        ip = *brackets_mapping.get(&ip).unwrap();
+                    }
+                }
                 _ => panic!("Should not happen")
             }
             ip += 1;
@@ -30,5 +132,11 @@ fn main() {
             break
         }
     }
-    println!("{sample}");
+}
+
+fn main() {
+    let sample = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++."; // prints "Z"
+    let mut io = InMemoryIO::default();
+    run_program(&sample, &mut io);
+    println!("{:?}", io);
 }
